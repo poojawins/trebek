33497d9f9f36fd4655ee381e3ec24237f8ad4df6
Pooja Pasawala <pooja@basicer.com>
2014-02-02 14:04:07 -0500

    Update files with working board and images

--- app.rb
+++ app.rb
@@ -13,7 +13,7 @@ class Gambit < Sinatra::Application
   end
 
   get "/styles.css" do
-    scss :trebek
+    scss :memory_game_pooja
   end
 
   get "/" do
---
+++ public/memory_game_pooja.js
@@ -0,0 +1,29 @@
+$(document).ready(function(){
+  var $card = $("div.card");
+  var$button = $(".button");
+  
+  $card.on("click", function(){
+    var $this = $(this);
+    var chosen;
+
+    $this.toggleClass("chosen");
+    $chosen = $(".chosen");
+
+    if($chosen.length === 2) {
+      
+    }
+    
+    if(!$this.hasClass("played") && !$this.hasClass("disabled")) {
+      if(!$this.hasClass("chosen")) {
+        $this.addClass("chosen");
+        $this.find(".color").fadeIn();
+        $this.find(".value").fadeOut();
+        $cards.not(".chosen").addClass("disabled");
+      });
+    });
+  });
+});
+
+  // $button.on("click", function(){
+  //   location.reload();
+  // });
