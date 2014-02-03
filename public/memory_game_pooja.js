3f89f2587d61201ea347c99e4e3cd719616134ad
Pooja Pasawala <pooja@basicer.com>
2014-02-02 19:48:05 -0500

    Add working play again button and add working click events though logic is off still

--- public/memory_game_pooja.js
+++ public/memory_game_pooja.js
@@ -2,32 +2,32 @@ $(document).ready(function(){
   var $card = $("div.card");
   var $button = $(".button");
 
-
-
-
   $card.on("click", function(){
     var $this = $(this);
     var $chosen;
     
-    $this.removeClass("hide");
-    $chosen = $(".chosen");
-    $this.toggleClass("chosen");
-    if($chosen.length === 2) {
-
-    }
-    
     if(!$this.hasClass("played") && !$this.hasClass("disabled")) {
       if(!$this.hasClass("chosen")) {
+        $this.find(".picture").removeClass("hide");
+        $chosen = $(".chosen");
         $this.addClass("chosen");
         $this.find(".picture").fadeIn();
-        $this.find(".value").fadeOut();
-        $cards.not(".chosen").addClass("disabled");
-      });
-    });
+        $this.find(".defaultpic").addClass("hide").fadeOut();
+        if($chosen.length === 2) {
+          if( $card.value === $card.value){
+            $this.addClass("disabled");
+            $this.removeClass("chosen");
+          } else{
+            $this.removeClass("chosen");
+            $this.addClass("hide");
+          }
+        }
+      }
+    }
   });
-
+    
   $button.on("click", function(){
-    location.reload();
+    window.location.reload();
   });
   
 });
