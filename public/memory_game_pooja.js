365f12ab5df7854c855c9d6baea7dcd120d3e74e
Pooja Pasawala <pooja@basicer.com>
2014-02-03 10:13:45 -0500

    Add working card match logic and cards shuffle

--- public/memory_game_pooja.js
+++ public/memory_game_pooja.js
@@ -6,26 +6,33 @@ $(document).ready(function(){
     var $this = $(this);
     var $chosen;
     
-    if(!$this.hasClass("played") && !$this.hasClass("disabled")) {
+    if(!$this.hasClass("played")) {
       if(!$this.hasClass("chosen")) {
-        $this.find(".picture").removeClass("hide");
-        $chosen = $(".chosen");
         $this.addClass("chosen");
-        $this.find(".picture").fadeIn();
+        $chosen = $(".chosen");
+        $this.find(".picture").removeClass("hide").fadeIn();
         $this.find(".defaultpic").addClass("hide").fadeOut();
-        if($chosen.length === 2) {
-          if( $card.value === $card.value){
-            $this.addClass("disabled");
-            $this.removeClass("chosen");
-          } else{
-            $this.removeClass("chosen");
-            $this.addClass("hide");
-          }
-        }
+        setTimeout(checkCards, 2000);
+      
       }
     }
   });
-    
+
+  function checkCards() {
+    var $chosen = $(".chosen");
+    if($chosen.length === 2) {
+      if($(".card.chosen").find(".value").eq(0).text() == $(".card.chosen").find(".value").eq(1).text()){
+        console.log("match!");
+        $chosen.addClass("played");
+        $chosen.removeClass("chosen");
+      } else{
+        $chosen.find(".picture").addClass("hide");
+        $chosen.find(".defaultpic").removeClass("hide");
+        $chosen.removeClass("chosen");
+      }
+    }
+  }
+
   $button.on("click", function(){
     window.location.reload();
   });
