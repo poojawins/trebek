1305231ab78e98a662a39da0d76359a38cfbf3d6
Pooja Pasawala <pooja@basicer.com>
2014-02-02 19:26:45 -0500

    Add hide class to puppy pics and remove category property

--- public/memory_game_pooja.js
+++ public/memory_game_pooja.js
@@ -1,14 +1,17 @@
 $(document).ready(function(){
   var $card = $("div.card");
-  var$button = $(".button");
+  var $button = $(".button");
+
+
+
 
   $card.on("click", function(){
     var $this = $(this);
     var $chosen;
     
+    $this.removeClass("hide");
     $chosen = $(".chosen");
     $this.toggleClass("chosen");
-    
     if($chosen.length === 2) {
 
     }
@@ -16,13 +19,16 @@ $(document).ready(function(){
     if(!$this.hasClass("played") && !$this.hasClass("disabled")) {
       if(!$this.hasClass("chosen")) {
         $this.addClass("chosen");
-        $this.find(".color").fadeIn();
+        $this.find(".picture").fadeIn();
         $this.find(".value").fadeOut();
         $cards.not(".chosen").addClass("disabled");
       });
     });
   });
 
+  $button.on("click", function(){
+    location.reload();
+  });
   
 });
 
