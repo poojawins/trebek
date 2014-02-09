b128f53c48a86e4edeff6d3e7ad138bb27ae7355
Pooja Pasawala <pooja@basicer.com>
2014-02-08 19:15:16 -0500

    Cleanup files

--- public/memory_game_pooja.js
+++ public/memory_game_pooja.js
@@ -5,15 +5,15 @@ $(document).ready(function(){
   $card.on("click", function(){
     var $this = $(this);
     var $chosen;
+    var $thiscounter = 0;
     
-    if(!$this.hasClass("played")) {
+    if(!$this.hasClass("played") && !$this.hasClass("disabled")) {
       if(!$this.hasClass("chosen")) {
         $this.addClass("chosen");
         $chosen = $(".chosen");
         $this.find(".picture").removeClass("hide").fadeIn();
         $this.find(".defaultpic").addClass("hide").fadeOut();
         setTimeout(checkCards, 2000);
-      
       }
     }
   });
@@ -22,12 +22,12 @@ $(document).ready(function(){
     var $chosen = $(".chosen");
     if($chosen.length === 2) {
       if($(".card.chosen").find(".value").eq(0).text() == $(".card.chosen").find(".value").eq(1).text()){
-        console.log("match!");
         $chosen.addClass("played");
         $chosen.removeClass("chosen");
       } else{
         $chosen.find(".picture").addClass("hide");
         $chosen.find(".defaultpic").removeClass("hide");
+        $card.find(".disabled").removeClass("disabled");
         $chosen.removeClass("chosen");
       }
     }
